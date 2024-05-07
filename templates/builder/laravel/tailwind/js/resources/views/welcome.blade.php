9e8015b9282c6655cbbd673fb13c941aa2c9f4d9
Adam Berecz <adam@laraform.io>
2024-05-07 12:13:13 +0200

    feat: added google places api script

--- templates/builder/laravel/tailwind/js/resources/views/welcome.blade.php
+++ templates/builder/laravel/tailwind/js/resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBCToIMP1Rk6ZbGcfnJ2iwsnb09_lnxLmY&libraries=places"></script>
 </head>
 <body>
     <div id="app"></div>
--- templates/vueform/nuxt/tailwind/ts/nuxt.config.ts
+++ templates/vueform/nuxt/tailwind/ts/nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      script: [
+        {
+          async: true,
+          src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBCToIMP1Rk6ZbGcfnJ2iwsnb09_lnxLmY&libraries=places',
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
